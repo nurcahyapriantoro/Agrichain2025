dbdea5f3dace413f1b2be4758a789e3f5272fe4d
Nurcahya Priantoro <80837061+nurcahyapriantoro@users.noreply.github.com>
2025-05-14 17:34:07 +0700

    Delete components directory

--- components/index.ts
+++
@@ -1,8 +0,0 @@
-// Export UI components
-export * from './ui/button';
-
-// Export home components
-export * from './home';
-
-// Export client copyright component
-export * from './client-copyright'; 
--- components/ui/toaster.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Toast, ToastClose, ToastDescription, ToastTitle } from "./toast";
-
-// A simplified toaster component that doesn't actually display toasts
-// This is just a placeholder to satisfy import requirements
-export function Toaster() {
-  // In a real implementation, this would render active toasts
-  return null;
-} 
